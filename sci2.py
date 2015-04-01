872bb324549ebb19e9335dd12487160dcc4b7858
Elias Lousseief <elias.lousseief@blackwinged-angel.com>
2015-04-01 13:04:59 +0200

    Added metrics statistical output

--- sci2.py
+++ sci2.py
@@ -4,15 +4,28 @@ import numpy
 import pandas
 import csv
 import feature_reduction
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import SGDClassifier
+
+#preprocessing
 from nltk.corpus import stopwords
 from nltk import word_tokenize          
 from nltk.stem import WordNetLemmatizer 
 from nltk.stem.snowball import EnglishStemmer
+
+#classifiers and classifier tools
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.svm import NuSVC
+from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
+from sklearn import metrics
+
+class StemTokenizer(object):
+	def __init__(self):
+		self.stemmer = EnglishStemmer()
+	def __call__(self, doc):
+		return [self.stemmer.stem(t) for t in word_tokenize(doc)]
 
 trainingData = pandas.DataFrame({'text': [], 'class': []})
 with open('training.csv') as csvfile:
@@ -29,20 +42,18 @@ with open('testing.csv') as csvfile:
 		if row['polarity'] in ('0', '4'):
 			testData = testData.append(pandas.DataFrame({'text': [feature_reduction.reduce(row['tweet'])], 'class': [row['polarity']]}, index=[row['id']]))
 
-class StemTokenizer(object):
-	def __init__(self):
-		self.stemmer = EnglishStemmer()
-	def __call__(self, doc):
-		return [self.stemmer.stem(t) for t in word_tokenize(doc)]
-
 stop_words = stopwords.words('english')
 stop_words.extend([feature_reduction.user_token.lower(), feature_reduction.url_token.lower()]);
 
 pipe = Pipeline([
 					('counter', CountVectorizer(ngram_range=(1, 1), stop_words=stop_words, tokenizer=StemTokenizer())), 
 					#('normalizer', TfidfTransformer(smooth_idf=True, sublinear_tf=False, use_idf=True)), #should we want to use a TfidfTransformer
-					('classifier', MultinomialNB(fit_prior=False, alpha=1.0))
-					#('classifier', SGDClassifier())
+					#('classifier', MultinomialNB(fit_prior=False, alpha=1.0))
+					#('classifier', SVC()) #default kernel is 'rbf', 'rbf' and 'poly' results in an error... why?
+					#('classifier', SVC(kernel='poly')) #error...
+					('classifier', SVC(kernel='linear')) #this one and the one below yield different results, why?
+					#('classifier', LinearSVC())
+					
 				])
 
 #applies fit and transform to first parameter at all stages but the last, where only fit is applied
@@ -51,14 +62,6 @@ pipe.fit(numpy.asarray(trainingData['text']), numpy.asarray(trainingData['class'
 #applies transform at all stages but the last one where predict is applied
 predictions = pipe.predict(numpy.asarray(testData['text']))
 
-print("Processed predictions: " + str(len(predictions)))
-count = 0
-for (i, sentiment) in enumerate(numpy.asarray(testData['class'])):
-	#print(str(predictions[i]) + ": " + sentiment)
-	if predictions[i] == sentiment:
-		count = count + 1
-
-print("count: " + str(count))
-print("predictions: " + str(len(predictions)))
-print(count / float(len(predictions)))
-#print(predictions)
+#http://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_recall_fscore_support.html for details of categories
+#total recall is the same value as the correctly predicted sentiments / all documents yields
+print(metrics.classification_report(numpy.asarray(testData['class']), predictions, target_names= [ 'Positive', 'Negative'], digits = 5 ))
