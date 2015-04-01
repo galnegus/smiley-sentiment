77cb61583d2731468c0aa0401b2358862247c8af
Elias Lousseief <elias.lousseief@blackwinged-angel.com>
2015-04-01 11:03:42 +0200

    Added pipeline

--- sci2.py
+++ sci2.py
@@ -5,11 +5,13 @@ import pandas
 import csv
 import feature_reduction
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 from nltk.corpus import stopwords
 from nltk import word_tokenize          
 from nltk.stem import WordNetLemmatizer 
 from nltk.stem.snowball import EnglishStemmer
+from sklearn.pipeline import Pipeline
 
 trainingData = pandas.DataFrame({'text': [], 'class': []})
 with open('training.csv') as csvfile:
@@ -34,16 +36,19 @@ class StemTokenizer(object):
 
 stop_words = stopwords.words('english')
 stop_words.extend([feature_reduction.user_token.lower(), feature_reduction.url_token.lower()]);
-count_vectorizer = CountVectorizer(ngram_range=(1, 1), stop_words=stop_words, tokenizer=StemTokenizer()) #how many times a certain word has to appear in the text to take it into account
-counts = count_vectorizer.fit_transform(numpy.asarray(trainingData['text']))
 
-classifier = MultinomialNB(fit_prior=False, alpha=1.0)
+pipe = Pipeline([
+					('counter', CountVectorizer(ngram_range=(1, 1), stop_words=stop_words, tokenizer=StemTokenizer())), 
+					#('normalizer', TfidfTransformer(smooth_idf=True, sublinear_tf=False, use_idf=True)), #should we want to use a TfidfTransformer
+					('classifier', MultinomialNB(fit_prior=False, alpha=1.0))
+				])
 
-targets = numpy.asarray(trainingData['class'])
-classifier.fit(counts, targets)
+#applies fit and transform to first parameter at all stages but the last, where only fit is applied
+pipe.fit(numpy.asarray(trainingData['text']), numpy.asarray(trainingData['class']))
 
-example_counts = count_vectorizer.transform(numpy.asarray(testData['text']))
-predictions = classifier.predict(example_counts)
+#applies transform at all stages but the last one where predict is applied
+predictions = pipe.predict(numpy.asarray(testData['text']))
+
 print("Processed predictions: " + str(len(predictions)))
 count = 0
 for (i, sentiment) in enumerate(numpy.asarray(testData['class'])):
